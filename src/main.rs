4201baa98441a922697c7602fa925228e949c6b4
liuhe <liuhe@zju.edu.cn>
2020-02-28 21:50:58 +0800

    使用结构体直接当作状态，并在多个状态之间跳转，这种思路是所谓的rust思路。

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,10 @@ fn main() {
     let mut post = Post::new();
 
     post.add_text("I ate a salad for lunch today");
-    // assert_eq!("", post.content());
-    println!("{}", post.content());
 
-    post.request_review();
-    // assert_eq!("", post.content());
-    println!("{}", post.content());
+    let post = post.request_review();
 
-    post.approve();
-    // assert_eq!("I ate a salad for lunch today", post.content());
-    println!("{}", post.content());
+    let post = post.approve();
+
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
